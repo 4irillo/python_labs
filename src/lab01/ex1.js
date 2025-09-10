e3d5c0eb25c2748bca8ca66e5b5031ace4ab1e7f
4irillo <ptashkir@gmail.com>
2025-09-10 18:08:04 +0300

    lab01/ex1 solved

--- src/lab01/ex1.js
+++ src/lab01/ex1.js
@@ -1,4 +1,4 @@
 const prompt = require('prompt-sync')(); // requires 'npm install prompt-sync'
-var a = prompt('Имя: ')
-var b = parseInt(prompt('Возраст: '))
-console.log('Привет, ${a}! Через год тебе будет ${b+1}.')
+let a = prompt('Имя: ');
+let b = parseInt(prompt('Возраст: '));
+console.log(`Привет, ${a}! Через год тебе будет ${b+1}.`);
