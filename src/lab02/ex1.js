7bcb41e95892defdb93394f3660131659f6a1fc8
4irillo <ptashkir@gmail.com>
2025-09-17 01:06:47 +0300

    Tasks 1 and 2 completed

---
+++ src/lab02/ex1.js
@@ -0,0 +1,5 @@
+import prompt from 'prompt-sync'// requires 'npm install prompt-sync'
+import * as tuple from '../../libs/tuples.js';
+//console.log(typeof([3, -1, 5, 5, 0]))
+let a = new tuple.Tuple("    Пташник       Кирилл      Леонидович       ", "BIVT-25-7", 5.00)
+console.log(a);
