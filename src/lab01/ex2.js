f2e939cdc327c082bdca808a81a5054424d137c8
4irillo <ptashkir@gmail.com>
2025-09-21 21:34:21 +0300

    works for linux

--- src/lab01/ex2.js
+++ src/lab01/ex2.js
@@ -1,4 +1,4 @@
 const prompt = require('prompt-sync')(); // requires 'npm install prompt-sync'
 var a = parseInt(prompt('a: '));
-var b = parseInt(prompt('a: '));
+var b = parseInt(prompt('b: '));
 console.log(`sum=${a+b}; avg=${(a+b)/2}`)
