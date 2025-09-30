df4f9b8d98b2a6d173ba12e617c4cbac66d32a98
4irillo <ptashkir@gmail.com>
2025-10-01 00:32:46 +0300

    No JS in Python

--- src/lab01/ex1.js
+++
@@ -1,4 +0,0 @@
-const prompt = require('prompt-sync')(); // requires 'npm install prompt-sync'
-let a = prompt('Имя: ');
-let b = parseInt(prompt('Возраст: '));
-console.log(`Привет, ${a}! Через год тебе будет ${b+1}.`);
--- src/lab01/ex2.js
+++
@@ -1,4 +0,0 @@
-const prompt = require('prompt-sync')(); // requires 'npm install prompt-sync'
-var a = parseInt(prompt('a: '));
-var b = parseInt(prompt('bck: '));
-console.log(`sum=${a+b}; avg=${(a+b)/2}`)
