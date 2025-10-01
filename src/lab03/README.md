de2c2636b67091c451cbf2a52e2fbeb47855a89a
4irillo <ptashkir@gmail.com>
2025-10-01 15:54:54 +0300

    lab03 done

---
+++ src/lab03/README.md
@@ -0,0 +1,4 @@
+# Ниже я приведу примеры функционирования моих замечательных функций корректно при любых корректных входных данных
+![output] (/home/kirill/Documents/vscode/python_labs/src/lab03/images/outputFunc.png)
+# Ниже я приведу пример абсолютно корректного функционирования реквеста
+![output] (/home/kirill/Documents/vscode/python_labs/src/lab03/images/image.png)
--- src/lab03/src/lib/text.py
+++ src/lab03/src/lib/text.py
@@ -19,14 +19,30 @@ def count_freq(tokens: list[str]) -> dict[str, int]:
             freq[token] += 1
         except:
             freq[token] = 1
-    return dict(sorted(freq.items()))
+    return dict(sorted(freq.items(), key=lambda item: (-item[1], item[0])))
 
 def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
     temp = []
-    freq = dict(sorted(freq.items()))
     cnt = 0
     for token in freq.items():
         temp.append(token)
         cnt += 1
-        if (cnt == n): break
+        if (cnt == n or cnt == len(freq)): break
     return temp
+
+'''print("normalize")
+print(f"'ПрИвЕт\nМИр\t' --> {normalize("ПрИвЕт\nМИр\t")}")
+print(f"'ёжик, Ёлка' --> {normalize("ёжик, Ёлка")}")
+print(f"'Hello\r\nWorld' --> {normalize("Hello\r\nWorld")}")
+print(f"'  двойные   пробелы  ' --> {normalize("  двойные   пробелы  ")}")
+print("\ntokenize")
+print(f"'привет мир' --> {tokenize("привет мир")}")
+print(f"'hello,world!!!' --> {tokenize("hello,world!!!")}")
+print(f"'по-настоящему круто' --> {tokenize("по-настоящему круто")}")
+print(f"'2025 год' --> {tokenize("2025 год" )}")
+print(f"'emoji 😀 не слово' --> {tokenize("emoji 😀 не слово")}")
+
+print("\ncount_freq+top_n")
+print(f"['a','b','a','c','b','a'] --> {count_freq(["a","b","a","c","b","a"])}; {top_n(count_freq(["a","b","a","c","b","a"]), n=2)}")
+print(f"['b','aa','bb','aa','cc'] --> {count_freq(["bb","aa","bb","aa","cc"])}; {top_n(count_freq(["bb","aa","bb","aa","cc"]), n=2)}")
+'''
