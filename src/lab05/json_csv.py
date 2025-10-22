337d5b1a4b72f8b52c138c202058e6b140cf156b
4irillo <ptashkir@gamil.com>
2025-10-22 14:58:46 +0300

    lab05 done

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -7,9 +7,9 @@ from pathlib import Path
 def json_to_csv(json_path: str, csv_path: str) -> None:
     try:
         with Path(json_path).open('r', encoding="utf-8") as f:
-            temp = json.load(f)
             if (not len(f)):
                 raise ValueError("Пустой JSON или неподдерживаемая структура")
+            temp = json.load(f)
     except:
         raise FileNotFoundError(f'No such .json file.')
     try: 
@@ -24,6 +24,8 @@ def json_to_csv(json_path: str, csv_path: str) -> None:
 def csv_to_json(csv_path: str, json_path: str) -> None:
     try: 
         with Path(csv_path).open('r', encoding="utf-8") as f:
+            if (not len(f)):
+               raise ValueError("Пустой JSON или неподдерживаемая структура")
             reader = csv.DictReader(f)
             temp = []
             for row in reader:
@@ -45,6 +47,8 @@ def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
     ws.title = "Sheet1"
     try: 
         with Path(csv_path).open('r', encoding="utf-8") as f:
+            if (not len(f)):
+                raise ValueError("Пустой JSON или неподдерживаемая структура")
             for row in csv.reader(f):
                 ws.append(row)
             wb.save(xlsx_path)
