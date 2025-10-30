f819541db955e245ddfbddfbfe782d0b559249b6
4irillo <ptashkir@gmail.com>
2025-10-31 01:34:41 +0300

    Lab05 done 100%

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -1,31 +1,30 @@
 import json
 import csv
-from openpyxl import Workbook
 from pathlib import Path
 
 
 def json_to_csv(json_path: str, csv_path: str) -> None:
     try:
         with Path(json_path).open('r', encoding="utf-8") as f:
-            if (not len(f)):
-                raise ValueError("Пустой JSON или неподдерживаемая структура")
             temp = json.load(f)
     except:
         raise FileNotFoundError(f'No such .json file.')
     try: 
         with Path(csv_path).open('w', encoding="utf-8") as f:
-            csv.writer('name, age, city')
-            csv.DictWriter(f, fieldnames=['name', 'age', 'city']).writerows(temp)
+            if not temp:
+                raise ValueError("Empty JSON or unsupported structure")
+            fieldnames = temp[0].keys()
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(temp)
     except:
         raise FileNotFoundError(f'No such .csv file.')
 
-#json_to_csv(json_path='/home/kirill/Documents/VScode/python_labs/data/lab05/samples/people.json', csv_path='/home/kirill/Documents/VScode/python_labs/data/lab05/out/people_from_json.csv')
+#json_to_csv(json_path='/home/kirill/Documents/VSC/python_labs/data/lab05/samples/people.json', csv_path='/home/kirill/Documents/VSC/python_labs/data/lab05/out/people_from_json.csv')
     
 def csv_to_json(csv_path: str, json_path: str) -> None:
     try: 
         with Path(csv_path).open('r', encoding="utf-8") as f:
-            if (not len(f)):
-               raise ValueError("Пустой JSON или неподдерживаемая структура")
             reader = csv.DictReader(f)
             temp = []
             for row in reader:
@@ -39,20 +38,5 @@ def csv_to_json(csv_path: str, json_path: str) -> None:
         raise FileNotFoundError(f'No such .json file.')
 
 
-#csv_to_json(csv_path='/home/kirill/Documents/VScode/python_labs/data/lab05/samples/people.csv', json_path='/home/kirill/Documents/VScode/python_labs/data/lab05/out/people_from_csv.json')
+#csv_to_json(csv_path='/home/kirill/Documents/VSC/python_labs/data/lab05/samples/people.csv', json_path='/home/kirill/Documents/VSC/python_labs/data/lab05/out/people_from_csv.json')
 
-def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Sheet1"
-    try: 
-        with Path(csv_path).open('r', encoding="utf-8") as f:
-            if (not len(f)):
-                raise ValueError("Пустой JSON или неподдерживаемая структура")
-            for row in csv.reader(f):
-                ws.append(row)
-            wb.save(xlsx_path)
-    except: 
-        raise FileNotFoundError(f'No such .csv file.')
-
-csv_to_xlsx(csv_path='/home/kirill/Documents/VScode/python_labs/data/lab05/samples/people.csv', xlsx_path='/home/kirill/Documents/VScode/python_labs/data/lab05/out/people.xlsx')
